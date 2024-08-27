13710ce402abfd47a5d114b250ae3a73a9794d88
arindam69x <147473276+arindam69x@users.noreply.github.com>
2024-08-28 01:19:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>1. First Join the channel \n2. Tap on Original link again or Reload ⚡️ \n3. Tap on Start and Done ✅ \nFor more help Join <a href=https://t.me/Alliance_Weebs>Support Group</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Main Channel : @Adult_Saga</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🍁 ᴄʟᴏꜱᴇ", callback_data = "close")
+                        InlineKeyboardButton("😴 Close", callback_data = "close")
                     ]
                 ]
             )
